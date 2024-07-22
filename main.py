0fc806c913375779c0814757083191d06f37e846
Felix G <76596162+API-Troubles@users.noreply.github.com>
2024-07-22 15:13:29 +0000

    Added graphing function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 starting_num = input("Enter starting number: ")
 
 # Checks
@@ -6,23 +8,43 @@ try:
 except ValueError:
     raise TypeError("You must input a valid number!") from None
 
+if starting_num == 1:
+    raise TypeError("The number cannot be 1.")
+
 
 def even(num: int) -> bool:
     return num % 2 == 0
     
 
 next_num: int = 0
-results: list[int] = []
+results: list[int] = [starting_num]
 
 
 next_num = starting_num
 
 while next_num != 1:
     if even(next_num):
-        results.append(next_num / 2)  # type: ignore
+        results.append(int(next_num / 2))  # type: ignore
         next_num = next_num / 2       # type: ignore
     else:
-        results.append(next_num * 3 + 1)
+        results.append(int(next_num * 3 + 1))
         next_num = next_num * 3 + 1
 
-print(results)
+print("Results below:")
+print(", ".join(str(i) for i in results))
+
+
+x = range(len(results))
+y = results
+
+plt.plot(x, y)
+
+# naming the x axis
+plt.xlabel('Iteration Count')
+# naming the y axis
+plt.ylabel('Result')
+
+
+plt.title(f'Collatz Conjecture - {starting_num}')
+
+plt.savefig(f'graphs/{starting_num}.png')
