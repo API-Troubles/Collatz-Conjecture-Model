4beef387a7c794db3237b006b71bcc5db1fdbc63
Felix G <76596162+API-Troubles@users.noreply.github.com>
2024-07-22 15:49:49 +0000

    Finished writing the code lol

--- goldbach.py
+++ goldbach.py
@@ -1,4 +1,41 @@
 import sympy
+import matplotlib.pyplot as plt
+
+starting_num = input("Enter starting number: ")
+
+# Checks
+try:
+    starting_num = int(starting_num)
+except ValueError:
+    raise TypeError("You must input a valid number!") from None
+
+if starting_num <= 2:
+    raise ValueError("The number cannot be smaller or equal to 2.")
+
+if starting_num % 2 != 0:
+    raise ValueError("The number must be odd.")
 
 
-print(sympy.isprime(10))
+
+def goldbach_conjecture(number):
+    for addend in range(2, number // 2 + 1):  # other half is redundant and uneeded
+        if sympy.isprime(addend) and sympy.isprime(number - addend): # Check if they off
+            return {"result": True, "addends": [addend, number - addend]}
+    return {"result": False, "addends": None}
+
+
+check_conjecture = goldbach_conjecture(starting_num)
+
+if check_conjecture["result"]:
+    print(f"""
+    {starting_num} obeys the goldbach conjecture! 
+    {' + '.join(str(i) for i in check_conjecture['addends'])} = {starting_num}
+    """)
+else:
+    print(f"""
+    Wait... {starting_num} doesn't obey the conjecture?!11??!?!
+    either I coded this wrong or you just answered a conjecture! 
+    Wow such famous! Autograph pls?!?
+    """)
+# No seriously if you did somehow prove it false thad'd be funny
+# You'll split the prize money w/ me right? Trust!
